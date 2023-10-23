059b8bcee926195393b8ef99d5964d260c22bdaa
Pranjal BHaskar <97526424+pranjalbhaskar-27@users.noreply.github.com>
2023-10-23 23:41:17 +0530

    Updates code to check if number is prime or not.

--- prime.js
+++ prime.js
@@ -6,3 +6,24 @@ for(let i = 0; i<=num; i++){
         count++;
     }
 }
+
+// new code
+function isPrime(number) {
+    if (number <= 1) {
+      return false;
+    }
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+
+  const number = 17; 
+  if (isPrime(number)) {
+    console.log(number + " is a prime number.");
+  } else {
+    console.log(number + " is not a prime number.");
+  }
