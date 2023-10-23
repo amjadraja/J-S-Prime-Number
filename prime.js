6c0285bdb9e10c68fa5f23c30f24b1b2c2b763e1
Pranjal BHaskar <97526424+pranjalbhaskar-27@users.noreply.github.com>
2023-10-23 23:44:28 +0530

    Updates code to not use Math.sqrt

--- prime.js
+++ prime.js
@@ -1,25 +1,25 @@
 // write a code to check whether num is prime ornot
-let num = 7;
-let count = 0;
-for(let i = 0; i<=num; i++){
-    if(num%i==0){
-        count++;
-    }
-}
+// let num = 7;
+// let count = 0;
+// for(let i = 0; i<=num; i++){
+//     if(num%i==0){
+//         count++;
+//     }
+// }
 
 // new code
 function isPrime(number) {
     if (number <= 1) {
       return false;
     }
-    for (let i = 2; i <= Math.sqrt(number); i++) {
+    for (let i = 2; i <= number / 2; i++) {
       if (number % i === 0) {
         return false;
       }
     }
     return true;
   }
-
+  
 
   const number = 17; 
   if (isPrime(number)) {
