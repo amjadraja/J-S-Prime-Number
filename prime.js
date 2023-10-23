089fc64a411540431692d579a0a1cefe34f7fba5
Pranjal BHaskar <97526424+pranjalbhaskar-27@users.noreply.github.com>
2023-10-23 23:48:04 +0530

    Update prime.js

--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
 // write a code to check whether num is prime ornot
-// let num = 7;
-// let count = 0;
-// for(let i = 0; i<=num; i++){
-//     if(num%i==0){
-//         count++;
-//     }
-// }
+let num = 7;
+let count = 0;
+for(let i = 0; i<=num; i++){
+    if(num%i==0){
+        count++;
+    }
+}
 
 // new code
 function isPrime(number) {
